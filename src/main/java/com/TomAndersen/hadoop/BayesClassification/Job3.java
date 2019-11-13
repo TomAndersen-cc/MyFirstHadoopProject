cdd6caadddad20e1986745f7f70c872597ad6f01
TomAndersen-cc <1040994898@qq.com>
2019-11-13 17:05:35 +0800

    MapReduce实现贝叶斯

--- src/main/java/com/TomAndersen/hadoop/BayesClassification/Job3.java
+++ src/main/java/com/TomAndersen/hadoop/BayesClassification/Job3.java
@@ -20,14 +20,17 @@ import java.util.HashMap;
  * @Version
  * @Date 2019/11/8
  * Job3：
- * 在Job3的run函数中读取Job2的输出路径下的HDFS文档，将其中的文档类别和单词总数作为键值对压入Job3
- * 类的静态HashMap中，读入的文档名为含有"part"，使用正则表达式进行匹配。即获取到了
- * <文档类别><单词总数>以及<文档类别><文档总数>的两个HashMap<String,String>
- * 输入路径：Job1的输出路径
+ * 在Job3的run函数中读取Job2的输出路径下的HDFS文档，将其中的文档类别和单词总数作为键值对压入Job3类的静态
+ * HashMap中，读入的文档名为含有"part"，可以使用正则表达式进行匹配。
+ * （改：直接读取Job2输出的part文档，因为分类最多也不会超出Reducetask设置的默认阈值上限，保险起见手动设置
+ * ReduceTask的数量，保证输出的part文件只有一个，直接读取指定文件，而不读取文件夹路径，读取文件夹路径还需
+ * 要进一步判断	，十分麻烦）
+ * <p>
+ * 输入路径：Job1的输出路径（MapReduce程序会自动忽略以下划线"_"开头的文件，不用担心_success文件）
  * 输出路径：自定义Job3输出路径，如：/OutPut/Job3/
  * Mapper：
- * 输入：<LongWritable，<文档类别   单词  sum>>
- * 输出：<文档类别 单词，概率p>
+ * 输入：<nullwritable，<文档类别 单词 sum>>
+ * 输出：<文档类别 单词，条件概率p>
  * 在Mapper中读取对应文档类别的单词总数，计算单词出现概率
  * No Reducer
  */
@@ -39,19 +42,19 @@ public class Job3 extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
 
+        // 获取配置信息
         Configuration configuration = new Configuration();
         // 读取Job2的输出文件，获取文档类别到单词总数的映射
         HashMap[] myMaps = BayesTools.getKeyValuesByReadFile(JobsInitiator.Job2_OutputPath + "part-r-00000",
-                configuration,"\t");
-
-        fileClassToSumOfWords = myMaps[0];// 因为知道文档输出类型及内容，所以直接取第一个Map即可
+                configuration, "\t");
+        // 因为知道文档输出类型及内容，所以直接取第一个Map即可
+        fileClassToSumOfWords = myMaps[0];
         // fileClassToSumOfFiles = myMaps[1];
 
         //下面是对Job3的配置
         String InputPath = args[0]; // 输入路径为Job1的输出路径
         String OutputPath = args[1];// 输出路径
-
-        Configuration configuration1 = new Configuration();
+        // 获取Job实例
         Job job = Job.getInstance(configuration, this.getClass().getName());
 
         // 设置输入格式
@@ -60,7 +63,8 @@ public class Job3 extends Configured implements Tool {
         job.setJarByClass(Job3.class);
         // 设置Mapper
         job.setMapperClass(Job3Mapper.class);
-        // No Combiner,no Reducer
+        // No Combiner,no Reducer，使用默认的Reducer
+        job.setNumReduceTasks(1);// 设置单个ReduceTask，确保输出文件只有一个，便于后面的Job进行读取
         // 设置Mapper的输出Key-value类型
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(DoubleWritable.class);

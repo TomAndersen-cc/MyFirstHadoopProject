661086e98bc0eb5b43d3d434abf7363d84e1edb5
TomAndersen-cc <1040994898@qq.com>
2019-11-14 15:18:58 +0800

    MapReduce实现贝叶斯

--- src/main/java/com/TomAndersen/hadoop/BayesClassification/JobsInitiator.java
+++ src/main/java/com/TomAndersen/hadoop/BayesClassification/JobsInitiator.java
@@ -11,10 +11,18 @@ import org.apache.hadoop.util.ToolRunner;
  * 用于启动所有的Job，本类中只进行作业调度
  */
 public class JobsInitiator {
+
+    // 单机测试下的本地文件系统相对路径
     public static final String Job1_OutputPath = "src/Output/Job1";
     public static final String Job2_OutputPath = "src/OutPut/Job2/";
     public static final String Job3_OutputPath = "src/OutPut/Job3/";
     public static final String Job4_OutputPath = "src/OutPut/Job4/";
+
+    /*// 集群测试下的HDFS文件路径
+    public static final String Job1_OutputPath = "/Output/Job1";
+    public static final String Job2_OutputPath = "/OutPut/Job2/";
+    public static final String Job3_OutputPath = "/OutPut/Job3/";
+    public static final String Job4_OutputPath = "/OutPut/Job4/";*/
 
     public static void main(String[] args) throws Exception {
         // args中有两个参数，第一个为训练集，第二个为测试集
@@ -36,23 +44,27 @@ public class JobsInitiator {
         }
 
         final String TranDataSetPath = args[0];
-        final String TestDataSetPath = args[0];
+        final String TestDataSetPath = args[1];
 
-        /*BayesTools.CheckOutputPath(Job1_OutputPath);//检查Job1输出路径是否为空
+        BayesTools.CheckOutputPath(Job1_OutputPath);//检查Job1输出路径是否为空
         int Job1exitCode = ToolRunner.run(new Job1(), new String[]{TranDataSetPath, Job1_OutputPath});
 
-        System.exit(Job1exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止*/
+        // System.exit(Job1exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止
 
 
-/*        BayesTools.CheckOutputPath(Job2_OutputPath);//检查Job2输出路径是否为空
+        BayesTools.CheckOutputPath(Job2_OutputPath);//检查Job2输出路径是否为空
         int Job2exitCode = ToolRunner.run(new Job2(), new String[]{TranDataSetPath, Job2_OutputPath});
 
-        System.exit(Job2exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止*/
+        //System.exit(Job2exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止
 
         BayesTools.CheckOutputPath(Job3_OutputPath);//检查Job2输出路径是否为空
         int Job3exitCode = ToolRunner.run(new Job3(), new String[]{Job1_OutputPath, Job3_OutputPath});
 
-        System.exit(Job3exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止
+        System.exit(Job1exitCode + Job2exitCode + Job3exitCode);// 当参数为0时表示正常终止JVM，为非0时表示异常终止
+
+        // 使用训练好的模型进行分类
+        // BayesTools.BayesClassifier(TestDataSetPath);
+        // 将建模和测试分为两个程序比较好，不放在这里运行了
 
     }
 }
